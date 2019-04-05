d74af37e3fdec9fe9649d4c12cdc1b3d0bb235cb
beatdjam <beat3526@gmail.com>
2019-04-06 02:30:46 +0900

    Add Calculate Main Activity

---
+++ app/src/main/java/com/example/beatdjam/learning_android_create_app/CalculatorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.beatdjam.learning_android_create_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
--- app/src/main/java/com/example/beatdjam/learning_android_create_app/MainActivity.kt
+++ app/src/main/java/com/example/beatdjam/learning_android_create_app/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.beatdjam.learning_android_create_app
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calculator.setOnClickListener {
+            startActivity(Intent(this, CalculatorActivity::class.java))
+        }
     }
 }
