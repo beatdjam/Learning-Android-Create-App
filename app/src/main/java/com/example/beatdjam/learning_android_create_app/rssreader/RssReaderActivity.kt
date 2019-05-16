542dd9fb3c94fa0b35d974e8f1653d36685289a7
beatdjam <beat3526@gmail.com>
2019-05-16 23:44:48 +0900

    Add RssReaderActivity

---
+++ app/src/main/java/com/example/beatdjam/learning_android_create_app/rssreader/RssReaderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beatdjam.learning_android_create_app.rssreader
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RssReaderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rss_reader)
+    }
+}
