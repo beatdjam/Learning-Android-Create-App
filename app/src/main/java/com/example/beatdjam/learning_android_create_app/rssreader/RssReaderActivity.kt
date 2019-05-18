d0fe675ca5e386c3b9808f95cf5cbcc313855de8
beatdjam <beat3526@gmail.com>
2019-05-19 00:34:21 +0900

    Implemented "Received new articles" notification

--- app/src/main/java/com/example/beatdjam/learning_android_create_app/rssreader/RssReaderActivity.kt
+++ app/src/main/java/com/example/beatdjam/learning_android_create_app/rssreader/RssReaderActivity.kt
@@ -1,5 +1,8 @@
 package com.example.beatdjam.learning_android_create_app.rssreader
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +12,7 @@ import android.support.v4.content.Loader
 import android.support.v7.widget.GridLayoutManager
 import com.example.beatdjam.learning_android_create_app.R
 import kotlinx.android.synthetic.main.activity_rss_reader.articles
+import java.util.concurrent.TimeUnit
 
 class RssReaderActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Rss> {
     override fun onCreateLoader(id: Int, args: Bundle?) = RssLoader(this)
@@ -30,5 +34,14 @@ class RssReaderActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Rss
         setContentView(R.layout.activity_rss_reader)
 
         supportLoaderManager.initLoader(1, null, this)
+
+        createChannel(this)
+        val fetchJob = JobInfo
+            .Builder(1, ComponentName(this, PollingJob::class.java))
+            .setPeriodic(TimeUnit.HOURS.toMillis(6))
+            .setPersisted(true)
+            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+            .build()
+        getSystemService(JobScheduler::class.java).schedule(fetchJob)
     }
 }
